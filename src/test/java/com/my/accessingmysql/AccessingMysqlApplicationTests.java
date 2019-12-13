231e5a52ec5e596c731239141b08b1dfa795ae1f
vimal <vimalsingh99.2@gmail.com>
2019-12-13 20:59:40 +0530

    Add starter code

---
+++ src/test/java/com/my/accessingmysql/AccessingMysqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.my.accessingmysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessingMysqlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
