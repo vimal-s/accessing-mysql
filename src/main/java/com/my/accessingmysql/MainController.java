a67af8085002d3591cf07ccadbcd980cda2e0099
vimal <vimalsingh99.2@gmail.com>
2019-12-15 08:32:22 +0530

    Create the request controller

---
+++ src/main/java/com/my/accessingmysql/MainController.java
@@ -0,0 +1,28 @@
+package com.my.accessingmysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path = "/demo")
+public class MainController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+
+        userRepository.save(user);
+
+        return "Saved";
+    }
+
+    @GetMapping(path = "/show")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
